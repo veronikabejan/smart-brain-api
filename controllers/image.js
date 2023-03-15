83e60341c5578581a7df14841027e4b0d912bf52
Veronica Ghiujan <veronikabejan@gmail.com>
2023-03-15 14:43:30 +0100

    Ready to deploy backend

---
+++ controllers/image.js
@@ -0,0 +1,35 @@
+const Clarifai = require('clarifai');
+
+const app = new Clarifai.App({
+ apiKey: 'fdf5d4ae51c34945a3c392be92b9fb53'
+});
+
+const handleApiCall = (req, res) => {
+	app.models
+		.predict(
+	        {id: 'face-detection',
+	         name: 'face-detection',
+	         version: '6dc7e46bc9124c5c8824be4822abe105',
+	         type: 'visual-detector',
+	        }, req.body.input)
+		.then(data => {
+			res.json(data);
+		})
+		.catch(err => res.status(400).json('unable to work with API'))
+}
+
+const handleImage = (req, res, db) => {
+	const { id } = req.body;
+	db('users').where('id', '=', id)
+	.increment('entries', 1)
+	.returning('entries')
+	.then(entries => {
+		res.json(entries[0].entries);
+	})
+	.catch(err => res.status(400).json('unable to get entries'))
+  }
+
+  module.exports = {
+  	handleImage,
+  	handleApiCall
+  }
